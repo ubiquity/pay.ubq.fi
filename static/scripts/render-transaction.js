5dd61b2be0c6bdc13f5cf33837a3b843929c0674
アレクサンダー.eth <gpg@pavlovcik.com>
2023-02-24 20:54:54 +0800

    enhancement(styles): general refinement with address shortening

--- static/scripts/render-transaction.js
+++ static/scripts/render-transaction.js
@@ -18,27 +18,50 @@
   // insert tx data into table
   const table = document.getElementsByTagName(`table`)[0];
 
+  const requestedAmountElement = insertTableData(table);
+
+  await renderTokenSymbol(table, requestedAmountElement);
+})();
+
+
+
+function insertTableData(table) {
   const requestedAmountElement = document.getElementById("transferDetails.requestedAmount");
 
-  document.getElementById("permit.permitted.token").textContent = txData.permit.permitted.token;
+  const toFull = document.querySelector("#For .full");
+  const toShort = document.querySelector("#For .short");
+  toFull.textContent = txData.transferDetails.to;
+  toShort.textContent = shortenAddress(txData.transferDetails.to);
+
+  // document.getElementById("permit.permitted.token").textContent = shortenAddress(txData.permit.permitted.token);
+
+  const tokenFull = document.querySelector("#Token .full");
+  const tokenShort = document.querySelector("#Token .short");
+  tokenFull.textContent = txData.permit.permitted.token;
+  tokenShort.textContent = shortenAddress(txData.permit.permitted.token);
+
+
   document.getElementById("permit.permitted.amount").textContent = txData.permit.permitted.amount / 1e18;
   document.getElementById("permit.nonce").textContent = txData.permit.nonce;
   document.getElementById("permit.deadline").textContent = txData.permit.deadline;
-  document.getElementById("transferDetails.to").textContent = txData.transferDetails.to;
   requestedAmountElement.textContent = txData.transferDetails.requestedAmount / 1e18;
   document.getElementById("owner").textContent = txData.owner;
   document.getElementById("signature").textContent = txData.signature;
 
   table.setAttribute(`data-details-rendered`, "true");
+  return requestedAmountElement;
+}
 
-
-
-  // read token symbol
-  const contract = await window.getContract(txData.permit.permitted.token)
+async function renderTokenSymbol(table, requestedAmountElement) {
+  const contract = await window.getContract(txData.permit.permitted.token);
   // const name = await contract.name();
   const symbol = await contract.symbol();
   // console.trace(symbol);
   table.setAttribute(`data-contract-loaded`, "true");
-  requestedAmountElement.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="https://etherscan.io/address/${txData.permit.permitted.token}">${txData.transferDetails.requestedAmount / 1e18} ${symbol}</a>`;
-  // document.getElementById("Token").style.display = "none";
-})();
+  // https://etherscan.io/token/0x6b175474e89094c44da98b954eedeac495271d0f
+  requestedAmountElement.innerHTML = `<a target="_blank" rel="noopener noreferrer" href="https://etherscan.io/token/${txData.permit.permitted.token}?a=${txData.owner}">${txData.transferDetails.requestedAmount / 1e18} ${symbol}</a>`;
+}
+
+function shortenAddress(address) {
+  return `${address.slice(0, 10)}...${address.slice(-8)}`;
+}
