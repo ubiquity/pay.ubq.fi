d5f0d96b64969e8853576a19598c34ca0731dbfc
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-04-07 09:42:55 +0000

    feat: approval check

--- static/scripts/pay.ts
+++ static/scripts/pay.ts
@@ -3,6 +3,8 @@ import { JsonRpcSigner } from "@ethersproject/providers";
 import { daiAbi, permit2Abi } from "./abis";
 import { TxType, txData } from "./render-transaction";
 
+const permit2Address = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
+
 const ErrorHandler = (error: any) => {
   const output = document.querySelector(`footer>code`) as Element;
   delete error.stack;
@@ -17,24 +19,30 @@ const connectWallet = async (): Promise<JsonRpcSigner> => {
 };
 
 const withdraw = async (signer: JsonRpcSigner, txData: TxType) => {
-  const permit2Address = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
   const permit2Contract = new ethers.Contract(permit2Address, permit2Abi, signer);
   await permit2Contract.permitTransferFrom(txData.permit, txData.transferDetails, txData.owner, txData.signature).catch((error: any) => ErrorHandler(error));
 };
 
-const fetchTreasury = async (): Promise<number> => {
+const fetchTreasury = async (): Promise<{ balance: number; allowance: number }> => {
   const tokenAddress = txData.permit.permitted.token;
   const tokenContract = new ethers.Contract(tokenAddress, daiAbi, new ethers.providers.Web3Provider((window as any).ethereum));
   const balance = await tokenContract.balanceOf(txData.owner);
-  return balance;
+  const allowance = await tokenContract.allowance(txData.owner, permit2Address);
+  return { balance, allowance };
 };
 
-const toggleStatus = async (balance: number) => {
+const toggleStatus = async (balance: number, allowance: number) => {
   const treasuryValue = document.querySelector(".treasury-value") as Element;
-  treasuryValue.innerHTML =
+  const balanceElem =
     balance >= Number(txData.permit.permitted.amount)
-      ? `<span id="value-green">$${ethers.utils.formatUnits(balance, 18)}</span`
-      : `<span id="value-red">$${ethers.utils.formatUnits(balance, 18)}</span`;
+      ? `<span id="value-green">$${ethers.utils.formatUnits(balance, 18)}</span>`
+      : `<span id="value-red">$${ethers.utils.formatUnits(balance, 18)}</span>`;
+  const arrowElem = `<span id="value-cyan">${balance > allowance ? ">" : balance < allowance ? "<" : "="}</span>`;
+  const allowanceElem =
+    allowance >= Number(txData.permit.permitted.amount)
+      ? `<span id="value-green">$${ethers.utils.formatUnits(allowance, 18)}</span>`
+      : `<span id="value-red">$${ethers.utils.formatUnits(allowance, 18)}</span>`;
+  treasuryValue.innerHTML = `<div id="treasury-flex">` + balanceElem + arrowElem + allowanceElem + `</div>`;
 };
 
 export const pay = async (): Promise<void> => {
@@ -53,20 +61,22 @@ export const pay = async (): Promise<void> => {
   claimButtonElem.addEventListener("click", async () => {
     try {
       const signer = await connectWallet();
-      const balance = await fetchTreasury();
-      await toggleStatus(balance);
-      if (balance >= Number(txData.permit.permitted.amount)) {
+      const { balance, allowance } = await fetchTreasury();
+      await toggleStatus(balance, allowance);
+      if (balance >= Number(txData.permit.permitted.amount) && allowance >= Number(txData.permit.permitted.amount)) {
         await withdraw(signer, txData);
+      } else if (balance >= Number(txData.permit.permitted.amount)) {
+        ErrorHandler("Error: Not enough allowance to claim.");
       } else {
-        ErrorHandler("Error: Not enough funds on treasury to claim");
+        ErrorHandler("Error: Not enough funds on treasury to claim.");
       }
     } catch (error: unknown) {
       console.error(error);
     }
   });
 
-  const balance = await fetchTreasury();
-  await toggleStatus(balance);
+  const { balance, allowance } = await fetchTreasury();
+  await toggleStatus(balance, allowance);
 
   // display commit hash
   const commit = await fetch("commit.txt");
