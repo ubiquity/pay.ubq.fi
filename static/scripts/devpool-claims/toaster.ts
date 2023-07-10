208aefeda2929e1fa9e085089b23f5714d23335c
アレクサンダー.eth <gpg@pavlovcik.com>
2023-07-11 02:22:15 +0900

    fix: toaster close event handler

--- static/scripts/devpool-claims/toaster.ts
+++ static/scripts/devpool-claims/toaster.ts
@@ -21,33 +21,36 @@ const toastIcons = {
   info: "fa-circle-info",
 };
 
-const removeToast = (toast: HTMLElement, timeoutId?: NodeJS.Timeout) => {
-  toast.classList.add("hide");
-  if (timeoutId) {
-    clearTimeout(timeoutId); // Clearing the timeout for the toast
-  }
-  setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
-};
-
-export const createToast = (icon: keyof typeof toastIcons, text: string) => {
+export function createToast(icon: keyof typeof toastIcons, text: string) {
   // Getting the icon and text for the toast based on the id passed
   const _icon = toastIcons[icon];
   const toast = document.createElement("li"); // Creating a new 'li' element for the toast
   toast.className = `toast .${_icon}`; // Setting the classes for the toast
+
   // Setting the inner HTML for the toast
-  toast.innerHTML = `<div class="column">
-<i class="fa-solid ${_icon}"></i>
-<span>${text}</span>
-</div>
-<i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+  toast.innerHTML = `<div class="column"><i class="fa-solid ${_icon}"></i><span>${text}</span></div>`;
+
+  // attaching a click event listener to the toast to remove it when the close icon is clicked
+  const i = document.createElement("i");
+  i.className = "fa-solid fa-xmark";
+  i.onclick = () => removeToast(toast, toastDetails.timeoutId);
+  toast.appendChild(i);
 
   notifications.appendChild(toast); // Append the toast to the notification ul
 
   // Setting a timeout to remove the toast after the specified duration
   toastDetails.timeoutId = setTimeout(() => removeToast(toast, toastDetails.timeoutId), toastDetails.timer);
-};
+}
 
-export const disableClaimButton = (triggerLoader = true) => {
+function removeToast(toast: HTMLElement, timeoutId?: NodeJS.Timeout) {
+  toast.classList.add("hide");
+  if (timeoutId) {
+    clearTimeout(timeoutId); // Clearing the timeout for the toast
+  }
+  setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
+}
+
+export function disableClaimButton(triggerLoader = true) {
   claimButton.disabled = true;
 
   // Adding this because not all disabling should trigger loading spinner
@@ -55,15 +58,15 @@ export const disableClaimButton = (triggerLoader = true) => {
     claimLoader?.classList.add("show-cl"), claimLoader?.classList.remove("hide-cl");
     claimIcon?.classList.add("hide-cl"), claimIcon?.classList.remove("show-cl");
   }
-};
+}
 
-export const enableClaimButton = () => {
+export function enableClaimButton() {
   claimButton.disabled = false;
   claimLoader?.classList.add("hide-cl"), claimLoader?.classList.remove("show-cl");
   claimIcon?.classList.add("show-cl"), claimIcon?.classList.remove("hide-cl");
-};
+}
 
-export const ErrorHandler = (error: any, errorMessage?: string) => {
+export function ErrorHandler(error: any, errorMessage?: string) {
   delete error.stack;
   let ErrorData = JSON.stringify(error, null, 2);
   if (errorMessage) {
@@ -74,4 +77,4 @@ export const ErrorHandler = (error: any, errorMessage?: string) => {
     const _errorMessage = parsedError?.error?.message ?? parsedError?.reason;
     createToast("error", `Error: ${_errorMessage}`);
   }
-};
+}
