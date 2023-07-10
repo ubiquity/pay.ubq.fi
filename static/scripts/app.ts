0778efe5a50bc5c45acf5b17259bcfe6e1ac609d
アレクサンダー.eth <gpg@pavlovcik.com>
2023-07-10 21:06:27 +0900

    fix: load commit hash in ui first

--- static/scripts/app.ts
+++ static/scripts/app.ts
@@ -1,11 +1,19 @@
-import { drawInit } from "./draw";
 import { pay } from "./pay";
 import { renderTransaction } from "./render-transaction";
 
-const init = async () => {
-  await drawInit();
-  await renderTransaction();
-  await pay();
-};
-
-init();
+(async function appAsyncWrapper() {
+  try {
+    // display commit hash
+    const commit = await fetch("commit.txt");
+    if (commit.ok) {
+      const commitHash = await commit.text();
+      const buildElement = document.querySelector(`#build a`) as HTMLAnchorElement;
+      buildElement.innerHTML = commitHash;
+      buildElement.href = `https://github.com/ubiquity/pay.ubq.fi/commit/${commitHash}`;
+    }
+    await renderTransaction();
+    await pay();
+  } catch (error) {
+    console.error(error);
+  }
+})();
