f3722d2a020f995a5f07f23605d303b146feee8f
rndquu <rndquu@gmail.com>
2023-08-09 12:08:46 +0300

    fix: fix permit invalidation

--- static/scripts/rewards/web3/invalidate-nonce.ts
+++ static/scripts/rewards/web3/invalidate-nonce.ts
@@ -6,5 +6,9 @@ import { nonceBitmap } from "./nonce-bitmap";
 export async function invalidateNonce(signer: ethers.providers.JsonRpcSigner, nonce: BigNumber): Promise<void> {
   const permit2Contract = new ethers.Contract(PERMIT2_ADDRESS, permit2Abi, signer);
   const { wordPos, bitPos } = nonceBitmap(nonce);
-  await permit2Contract.invalidateUnorderedNonces(wordPos, bitPos);
+  // mimics https://github.com/ubiquity/pay.ubq.fi/blob/c9e7ed90718fe977fd9f348db27adf31d91d07fb/scripts/solidity/test/Permit2.t.sol#L428
+  const bit = BigNumber.from(1).shl(bitPos);
+  const sourceBitmap = await permit2Contract.nonceBitmap(await signer.getAddress(), wordPos.toString());
+  const mask = sourceBitmap.or(bit);
+  await permit2Contract.invalidateUnorderedNonces(wordPos, mask);
 }
