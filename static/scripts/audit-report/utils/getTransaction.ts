f30d2d1ec63ef0249bbe1181af9737c7d3de011d
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-08-28 11:57:11 +0000

    fix: remove unnecessary rpc calls

---
+++ static/scripts/audit-report/utils/getTransaction.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { ITransaction } from "../types/transaction";
+import { getBlockInfo, updateBlockInfo } from "./blockInfo";
+import { Chain } from "../constants";
+
+export const getTxInfo = async (hash: string, url: string, chain: Chain): Promise<ITransaction> => {
+  try {
+    const transactionResponse = await axios.post(url, {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "eth_getTransactionByHash",
+      params: [hash],
+    });
+    const transaction = transactionResponse.data.result as ITransaction;
+
+    let timestamp = await getBlockInfo(transaction.blockNumber, chain);
+    if (timestamp !== null) {
+      transaction.timestamp = timestamp;
+    } else {
+      const blockResponse = await axios.post(url, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_getBlockByNumber",
+        params: [transaction.blockNumber, false],
+      });
+      transaction.timestamp = blockResponse.data.result.timestamp;
+      updateBlockInfo(transaction.blockNumber, transaction.timestamp, chain);
+    }
+
+    return transaction;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
