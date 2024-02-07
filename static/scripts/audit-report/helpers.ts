982431f68ad9279510f41f60973326ae09e9ddb3
barebind <22911469+barebind@users.noreply.github.com>
2024-02-07 19:38:36 +0900

    Add optional rpc selection

--- static/scripts/audit-report/helpers.ts
+++ static/scripts/audit-report/helpers.ts
@@ -1,15 +1,38 @@
 import { ethers } from "ethers";
 import { API_KEYS, Chain, ChainScan, RPC_URLS } from "./constants";
 import { BountyHunter, GitHubUrlParts } from "./types";
+import axios from "axios";
 
 export interface RateLimitOptions {
-  method: string, url: string
+  method: string;
+  url: string;
 }
 
 const resultTableTbodyElem = document.querySelector("#resultTable tbody") as HTMLTableCellElement;
 
+type DataType = {
+  jsonrpc: string;
+  id: number;
+  result: {
+    number: string;
+    timestamp: string;
+    hash: string;
+  };
+};
+
+const RPC_BODY = JSON.stringify({
+  jsonrpc: "2.0",
+  method: "eth_getBlockByNumber",
+  params: ["latest", false],
+  id: 1,
+});
+
+const RPC_HEADER = {
+  "Content-Type": "application/json",
+};
+
 export const shortenTransactionHash = (hash: string | undefined, length = 8): string => {
-  if(!hash) return ""
+  if (!hash) return "";
   const prefixLength = Math.floor(length / 2);
   const suffixLength = length - prefixLength;
 
@@ -19,8 +42,17 @@ export const shortenTransactionHash = (hash: string | undefined, length = 8): st
   return prefix + "..." + suffix;
 };
 
-export const populateTable = (owner: string, repo: string, issue_number: number, network: string, txHash: string, issue_title: string, amount: string, bounty_hunter: BountyHunter) => {
-  if(!txHash) return; // permit not claimed
+export const populateTable = (
+  owner: string,
+  repo: string,
+  issue_number: number,
+  network: string,
+  txHash: string,
+  issue_title: string,
+  amount: string,
+  bounty_hunter: BountyHunter,
+) => {
+  if (!txHash) return; // permit not claimed
   const issue_url = `https://github.com/${owner}/${repo}/issues/${issue_number}`;
   const tx_url = `https://${getChainScan(network)}/tx/${txHash}`;
   const rows = `
@@ -28,18 +60,20 @@ export const populateTable = (owner: string, repo: string, issue_number: number,
         <td><a href="https://github.com/${owner}/${repo}" target="_blank">${owner}/${repo}</a></td>
         <td><a href="${issue_url}" target="_blank">#${issue_number} - ${issue_title}</a></td>
         <td><a href="${bounty_hunter?.url}" target="_blank">${bounty_hunter?.name}</a></td>
-        <td><a href="${tx_url}" target="_blank">${ethers.BigNumber.isBigNumber(amount) ? ethers.utils.formatEther(amount) :  amount} ${network === Chain.Ethereum ? "DAI" : "WXDAI"}</a></td>
+        <td><a href="${tx_url}" target="_blank">${ethers.BigNumber.isBigNumber(amount) ? ethers.utils.formatEther(amount) : amount} ${
+    network === Chain.Ethereum ? "DAI" : "WXDAI"
+  }</a></td>
         <td><a href="${tx_url}" target="_blank">${shortenTransactionHash(txHash)}</a></td>
     </tr>`;
 
   resultTableTbodyElem.insertAdjacentHTML("beforeend", rows);
-}
+};
 
 export const getChainScan = (chain: string) => {
-  return chain === Chain.Ethereum ? ChainScan.Ethereum : ChainScan.Gnosis
-}
+  return chain === Chain.Ethereum ? ChainScan.Ethereum : ChainScan.Gnosis;
+};
 
-export const getRandomAPIKey = (chain: Chain): string  => {
+export const getRandomAPIKey = (chain: Chain): string => {
   const keys = API_KEYS[chain];
   if (!keys || keys.length === 0) {
     throw new Error(`No API Keys found for chain: ${chain}`);
@@ -47,7 +81,7 @@ export const getRandomAPIKey = (chain: Chain): string  => {
 
   const randomIndex = Math.floor(Math.random() * keys.length);
   return keys[randomIndex];
-}
+};
 
 export const getRandomRpcUrl = (chain: Chain): string => {
   const urls = RPC_URLS[chain];
@@ -57,7 +91,46 @@ export const getRandomRpcUrl = (chain: Chain): string => {
 
   const randomIndex = Math.floor(Math.random() * urls.length);
   return urls[randomIndex];
-}
+};
+
+const verifyBlock = (data: DataType) => {
+  try {
+    const { jsonrpc, id, result } = data;
+    const { number, timestamp, hash } = result;
+    return jsonrpc === "2.0" && id === 1 && parseInt(number, 16) > 0 && parseInt(timestamp, 16) > 0 && hash.match(/[0-9|a-f|A-F|x]/gm)?.join("").length === 66;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const getOptimalRPC = async (chain: Chain): Promise<string> => {
+  const promises = RPC_URLS[chain].map(async (baseURL: string) => {
+    try {
+      const startTime = performance.now();
+      const API = axios.create({
+        baseURL,
+        headers: RPC_HEADER,
+      });
+
+      const { data } = await API.post("", RPC_BODY);
+      const endTime = performance.now();
+      const latency = endTime - startTime;
+      if (await verifyBlock(data)) {
+        return Promise.resolve({
+          latency,
+          baseURL,
+        });
+      } else {
+        return Promise.reject();
+      }
+    } catch (error) {
+      return Promise.reject();
+    }
+  });
+
+  const { baseURL: optimalRPC } = await Promise.any(promises);
+  return optimalRPC;
+};
 
 export const parseRepoUrl = (issueUrl: string): [string, string] => {
   const match = issueUrl.match(/github\.com\/([^/]+)\/([^/]+)\/issues\/\d+/i);
@@ -84,7 +157,7 @@ export const getGitHubUrlPartsArray = (urls: string[]): GitHubUrlParts[] => {
   }
 
   return githubUrlPartsArray;
-}
+};
 
 export const primaryRateLimitHandler = (retryAfter: number, options: RateLimitOptions) => {
   console.warn(`Request quota exhausted for request ${options.method} ${options.url}\nRetrying after ${retryAfter} seconds!`);
@@ -97,19 +170,18 @@ export const secondaryRateLimitHandler = (retryAfter: number, options: RateLimit
 };
 
 export const isValidUrl = (urlString: string) => {
-    try { 
-      return Boolean(new URL(urlString)); 
-    }
-    catch(e){ 
-      return false; 
-    }
-}
+  try {
+    return Boolean(new URL(urlString));
+  } catch (e) {
+    return false;
+  }
+};
 
 export const getCurrency = (comment: string) => {
-  if (comment.includes('WXDAI')) {
+  if (comment.includes("WXDAI")) {
     return Chain.Gnosis;
-  } else if (comment.includes('DAI')) {
+  } else if (comment.includes("DAI")) {
     return Chain.Ethereum;
   }
   return null;
-}
+};
