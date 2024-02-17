9be690849313f28473259989be6cea447b37c228
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-02-17 17:49:58 +0000

    chore: blockinfo.ts lint

--- static/scripts/audit-report/utils/blockInfo.ts
+++ static/scripts/audit-report/utils/blockInfo.ts
@@ -1,9 +1,9 @@
 import { Chain, CHAIN_MAP } from "../constants";
 
-export const getBlockInfo = async (blockNumber: string, chain: Chain) => {
-  return await localStorage.getItem(`${CHAIN_MAP[chain]}:${blockNumber}`);
-};
+export async function getBlockInfo(blockNumber: string, chain: Chain) {
+  return localStorage.getItem(`${CHAIN_MAP[chain]}:${blockNumber}`);
+}
 
-export const updateBlockInfo = async (blockNumber: string, timestamp: string, chain: Chain) => {
-  await localStorage.setItem(`${CHAIN_MAP[chain]}:${blockNumber}`, timestamp);
-};
+export async function updateBlockInfo(blockNumber: string, timestamp: string, chain: Chain) {
+  localStorage.setItem(`${CHAIN_MAP[chain]}:${blockNumber}`, timestamp);
+}
