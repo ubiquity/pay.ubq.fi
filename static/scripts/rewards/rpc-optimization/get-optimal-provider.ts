40b8c3e0c2cd3d058a7b3fad241408ce55895e00
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-02-27 18:09:40 +0000

    chore: no rpc mapping for localhost

--- static/scripts/rewards/rpc-optimization/get-optimal-provider.ts
+++ static/scripts/rewards/rpc-optimization/get-optimal-provider.ts
@@ -10,6 +10,12 @@ export async function useFastestRpc(app: AppState): Promise<JsonRpcProvider> {
   const networkId = app.reward.networkId || app.networkId || app.claims[0].networkId;
   if (!networkId) throw new Error("Network ID not found");
 
+  if (networkId === 31337)
+    return new JsonRpcProvider("http://127.0.0.1:8545", {
+      name: "http://127.0.0.1:8545",
+      chainId: 31337,
+    });
+
   if (!isTestCompleted && !isTestStarted) {
     isTestStarted = true;
     await testRpcPerformance(networkId).catch(console.error);
