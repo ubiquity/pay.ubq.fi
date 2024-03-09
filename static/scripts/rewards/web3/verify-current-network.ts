8697cf1d832b85c0ff02078880c3d8b17d075006
アレクサンダー.eth <gpg@pavlovcik.com>
2024-03-10 06:23:22 +0900

    fix: metamask not detected modals

--- static/scripts/rewards/web3/verify-current-network.ts
+++ static/scripts/rewards/web3/verify-current-network.ts
@@ -1,12 +1,11 @@
 import { ethers } from "ethers";
-import { buttonController, toaster } from "../toaster";
+import { buttonController } from "../toaster";
 import { handleIfOnCorrectNetwork } from "./handle-if-on-correct-network";
 import { notOnCorrectNetwork } from "./not-on-correct-network";
 
 // verifyCurrentNetwork checks if the user is on the correct network and displays an error if not
 export async function verifyCurrentNetwork(desiredNetworkId: number) {
   if (!window.ethereum) {
-    toaster.create("error", "MetaMask not detected. Please install MetaMask.");
     buttonController.hideAll();
     return;
   }
