541c6921830402e7c47d6ac997a6640fc887dc33
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-05-01 14:00:02 +0100

    feat: help modal

--- static/scripts/rewards/account-abstraction/webauthn.ts
+++ static/scripts/rewards/account-abstraction/webauthn.ts
@@ -3,8 +3,8 @@ import { generateSAPrivateKey } from "./sodium";
 import { app } from "../app-state";
 
 import { ethers, randomBytes } from "ethers";
-import { SafeAuthPack, SafeAuthConfig, SafeAuthInitOptions, AuthKitBasePack, AuthKitSignInData } from "@safe-global/auth-kit";
-import { EthersAdapter } from "@safe-global/protocol-kit";
+// import { SafeAuthPack, SafeAuthConfig, SafeAuthInitOptions, AuthKitBasePack, AuthKitSignInData } from "@safe-global/auth-kit";
+// import { EthersAdapter } from "@safe-global/protocol-kit";
 
 export async function webAuthn() {
   const isAvailable = await window.PublicKeyCredential.isConditionalMediationAvailable();
@@ -31,11 +31,10 @@ export async function webAuthn() {
   </tr>
   <tr>
   <th>
-  <div>Remember Me</div>
   </th>
   <td>
   <div class="button-container">
-  <input name="rememberMe" id="loginform.rememberMe" type="checkbox" />
+  <button class="help-button" type="button">Help</button>
   <button type="submit">Login</button>
   </div>
   </td>
@@ -44,7 +43,70 @@ export async function webAuthn() {
 
   thead.innerHTML = newHtml;
 
-  const button = tableElement.getElementsByTagName(`button`)[0];
+  const helpModal = document.createElement("div");
+  helpModal.id = "help-modal";
+  helpModal.innerHTML = `
+  <div id="help-modal" >
+  <div class="modal-content" data-modal-close="false">
+  <span class="close">&times;</span>
+  <h2>Ubiquity Rewards Help</h2>
+  <br/>
+  <ul>
+    <p>
+    After focusing on the input field, you will be prompted to use a passkey to create an account.
+    </p>
+    <p>
+    If you have already created an account, you can use the same passkey to login.
+    <p>
+    To create an account, ignore the passkey prompt and enter your username, it could be your GitHub username or any other username you prefer.
+    </p>
+    <p>
+    After entering your username, click on the login button and your new Safe account will be created.
+    </p>
+    <p>
+    You can use the new passkey to login across multiple devices, create multiple keys for one account or create multiple accounts.
+    </p>
+    <p>
+    We do not store your passkey, private key or any other sensitive information. We do create a unique identifier for your account which is stored in your browser.
+    </p>
+    <br/>
+  <p>
+  If you have any questions, please contact us on Discord.
+  </p>
+  </div>
+  </div>
+  `;
+
+  const helpButton = tableElement.getElementsByClassName(`help-button`)[0] as HTMLButtonElement;
+  if (!helpButton) {
+    throw new Error("Help button element not found");
+  }
+
+  const mainElement = document.getElementsByTagName(`main`)[0];
+  if (!mainElement) {
+    throw new Error("Main element not found");
+  }
+  const modalContent = helpModal.getElementsByClassName(`modal-content`)[0] as HTMLDivElement;
+
+  helpButton.addEventListener("click", () => {
+    console.log("clicked help button");
+    modalContent.setAttribute("data-modal-close", "false");
+    helpModal.classList.add("modal");
+
+    mainElement.appendChild(helpModal);
+  });
+
+  const close = helpModal.getElementsByClassName(`close`)[0] as HTMLSpanElement;
+  if (!close) {
+    throw new Error("Close element not found");
+  }
+
+  close.addEventListener("click", () => {
+    modalContent.setAttribute("data-modal-close", "true");
+    helpModal.remove();
+  });
+
+  const button = tableElement.getElementsByTagName(`button`)[1];
   if (!button) {
     throw new Error("Button element not found");
   }
@@ -81,7 +143,7 @@ export async function webAuthn() {
            */
           button.addEventListener("click", async () => {
             const username = (document.getElementById("loginform.username") as HTMLInputElement).value;
-            const rememberMe = (document.getElementById("loginform.rememberMe") as HTMLInputElement).checked;
+            // const rememberMe = (document.getElementById("loginform.rememberMe") as HTMLInputElement).checked;
 
             if (!username) {
               alert("Username is required");
@@ -106,7 +168,7 @@ export async function webAuthn() {
 
             const { id } = cred as PublicKeyCredential;
 
-            localStorage.setItem("ubqfi_acc", JSON.stringify({ id }));
+            localStorage.setItem("ubqfi_acc", JSON.stringify({ username, id }));
 
             const hasCreds = await webAuthn();
 
