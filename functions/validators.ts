e28ac1372f9abfe3c78c79d64e05b11ce5398666
EresDev <eresdev@hotmail.com>
2024-05-23 15:16:40 +0500

    fix: use reloadly sandbox by default

--- functions/validators.ts
+++ functions/validators.ts
@@ -12,9 +12,6 @@ export function validateRequestMethod(expectedMethod: string, receivedMethod: st
 }
 
 export function validateEnvVars(ctx) {
-  if (typeof ctx.env.USE_RELOADLY_SANDBOX != "boolean") {
-    throw new Error("USE_RELOADLY_SANDBOX env var must be set to boolean true or false.");
-  }
   if (!(ctx.env.RELOADLY_API_CLIENT_ID && ctx.env.RELOADLY_API_CLIENT_SECRET)) {
     console.error("One or more environment variable is missing.");
     throw new Error("Missing server configurations.");
