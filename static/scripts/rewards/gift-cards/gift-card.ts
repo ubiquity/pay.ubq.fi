1bb6211e13a5728a91eb2e42fc06f5f8c34a9594
EresDev <eresdev@hotmail.com>
2024-05-25 23:20:36 +0500

    fix: improve light mode support for gift cards

--- static/scripts/rewards/gift-cards/gift-card.ts
+++ static/scripts/rewards/gift-cards/gift-card.ts
@@ -33,7 +33,7 @@ export function getGiftCardHtml(giftCard: GiftCard, allowBuy: boolean, rewardAmo
               enable-background="new 0 0 0 0"
               xml:space="preserve"
             >
-              <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"></path>
+              <path fill="currentColor" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"></path>
             </svg>
           </span>
         </button>`
