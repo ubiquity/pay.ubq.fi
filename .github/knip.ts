eae313ef9d40f6612ee8b27695af1dc1bacf6bc4
アレクサンダー.eth <gpg@pavlovcik.com>
2024-07-19 13:59:39 +0900

    fix: knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -3,11 +3,12 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["build/index.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
+  ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "lib/**/*"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data"],
+  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier"],
   eslint: true,
+  ignoreBinaries: ["forge"],
 };
 
 export default config;
