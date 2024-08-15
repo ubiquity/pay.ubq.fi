4e757a6268d37ba700ecb4ee1564ff7f70d04de3
EresDev <eresdev@hotmail.com>
2024-08-16 01:43:11 +0500

    chore: add reloadly cards & countries lists

---
+++ functions/reloadly-lists.ts
@@ -0,0 +1,981 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+// Keep duplicate country names in different lists
+
+export const fallbackInternationalMastercard = {
+  country: "United States",
+  countryCode: "US",
+  name: "Virtual MasterCard International USD US",
+  sku: 18597,
+};
+
+export const fallbackInternationalVisa = {
+  country: "United States",
+  countryCode: "US",
+  name: "Visa International USD (Virtual) US",
+  sku: 18598,
+};
+
+export const mastercardInternationalSkus = [
+  {
+    country: "United States",
+    countryCode: "US",
+    name: "Virtual MasterCard International USD US",
+    sku: 18597,
+  },
+  {
+    country: "Algeria",
+    countryCode: "DZ",
+    name: "Mastercard Prepaid USD Debit (Virtual only) DZ",
+    sku: 18630,
+  },
+  {
+    country: "Australia",
+    countryCode: "AU",
+    name: "Mastercard Prepaid USD Debit (Virtual only) AU",
+    sku: 18631,
+  },
+  {
+    country: "Austria",
+    countryCode: "AT",
+    name: "Mastercard Prepaid USD Debit (Virtual only) AT",
+    sku: 18632,
+  },
+  {
+    country: "Bangladesh",
+    countryCode: "BD",
+    name: "Mastercard Prepaid USD Debit (Virtual only) BD",
+    sku: 18633,
+  },
+  {
+    country: "Belgium",
+    countryCode: "BE",
+    name: "Mastercard Prepaid USD Debit (Virtual only) BE",
+    sku: 18634,
+  },
+  {
+    country: "Brazil",
+    countryCode: "BR",
+    name: "Mastercard Prepaid USD Debit (Virtual only) BR",
+    sku: 18635,
+  },
+  {
+    country: "Canada",
+    countryCode: "CA",
+    name: "Mastercard Prepaid USD Debit (Virtual only) CA",
+    sku: 18636,
+  },
+  {
+    country: "Colombia",
+    countryCode: "CO",
+    name: "Mastercard Prepaid USD Debit (Virtual only) CO",
+    sku: 18637,
+  },
+  {
+    country: "Dominica",
+    countryCode: "DM",
+    name: "Mastercard Prepaid USD Debit (Virtual only) DM",
+    sku: 18638,
+  },
+  {
+    country: "Ecuador",
+    countryCode: "EC",
+    name: "Mastercard Prepaid USD Debit (Virtual only) EC",
+    sku: 18639,
+  },
+  {
+    country: "Egypt",
+    countryCode: "EG",
+    name: "Mastercard Prepaid USD Debit (Virtual only) EG",
+    sku: 18640,
+  },
+  {
+    country: "Finland",
+    countryCode: "FI",
+    name: "Mastercard Prepaid USD Debit (Virtual only) FI",
+    sku: 18641,
+  },
+  {
+    country: "France",
+    countryCode: "FR",
+    name: "Mastercard Prepaid USD Debit (Virtual only) FR",
+    sku: 18642,
+  },
+  {
+    country: "Germany",
+    countryCode: "DE",
+    name: "Mastercard Prepaid USD Debit (Virtual only) DE",
+    sku: 18643,
+  },
+  {
+    country: "Greece",
+    countryCode: "GR",
+    name: "Mastercard Prepaid USD Debit (Virtual only) GR",
+    sku: 18644,
+  },
+  {
+    country: "India",
+    countryCode: "IN",
+    name: "Mastercard Prepaid USD Debit (Virtual only) IN",
+    sku: 18645,
+  },
+  {
+    country: "Indonesia",
+    countryCode: "ID",
+    name: "Mastercard Prepaid USD Debit (Virtual only) ID",
+    sku: 18646,
+  },
+  {
+    country: "Ireland",
+    countryCode: "IE",
+    name: "Mastercard Prepaid USD Debit (Virtual only) IE",
+    sku: 18647,
+  },
+  {
+    country: "Italy",
+    countryCode: "IT",
+    name: "Mastercard Prepaid USD Debit (Virtual only) IT",
+    sku: 18648,
+  },
+  {
+    country: "Jamaica",
+    countryCode: "JM",
+    name: "Mastercard Prepaid USD Debit (Virtual only) JM",
+    sku: 18649,
+  },
+  {
+    country: "Jordan",
+    countryCode: "JO",
+    name: "Mastercard Prepaid USD Debit (Virtual only) JO",
+    sku: 18650,
+  },
+  {
+    country: "Malaysia",
+    countryCode: "MY",
+    name: "Mastercard Prepaid USD Debit (Virtual only) MY",
+    sku: 18651,
+  },
+  {
+    country: "Mexico",
+    countryCode: "MX",
+    name: "Mastercard Prepaid USD Debit (Virtual only) MX",
+    sku: 18652,
+  },
+  {
+    country: "Netherlands",
+    countryCode: "NL",
+    name: "Mastercard Prepaid USD Debit (Virtual only) NL",
+    sku: 18653,
+  },
+  {
+    country: "Philippines",
+    countryCode: "PH",
+    name: "Mastercard Prepaid USD Debit (Virtual only) PH",
+    sku: 18654,
+  },
+  {
+    country: "Poland",
+    countryCode: "PL",
+    name: "Mastercard Prepaid USD Debit (Virtual only) PL",
+    sku: 18655,
+  },
+  {
+    country: "Portugal",
+    countryCode: "PT",
+    name: "Mastercard Prepaid USD Debit (Virtual only) PT",
+    sku: 18656,
+  },
+  {
+    country: "Qatar",
+    countryCode: "QA",
+    name: "Mastercard Prepaid USD Debit (Virtual only) QA",
+    sku: 18657,
+  },
+  {
+    country: "Saudi Arabia",
+    countryCode: "SA",
+    name: "Mastercard Prepaid USD Debit (Virtual only) SA",
+    sku: 18658,
+  },
+  {
+    country: "Singapore",
+    countryCode: "SG",
+    name: "Mastercard Prepaid USD Debit (Virtual only) SG",
+    sku: 18659,
+  },
+  {
+    country: "Spain",
+    countryCode: "ES",
+    name: "Mastercard Prepaid USD Debit (Virtual only) ES",
+    sku: 18660,
+  },
+  {
+    country: "Thailand",
+    countryCode: "TH",
+    name: "Mastercard Prepaid USD Debit (Virtual only) TH",
+    sku: 18661,
+  },
+  {
+    country: "United Arab Emirates",
+    countryCode: "AE",
+    name: "Mastercard Prepaid USD Debit (Virtual only) AE",
+    sku: 18662,
+  },
+  {
+    country: "United Kingdom",
+    countryCode: "GB",
+    name: "Mastercard Prepaid USD Debit (Virtual only) GB",
+    sku: 18663,
+  },
+  {
+    country: "United Arab Emirates",
+    countryCode: "AE",
+    name: "Mastercard Prepaid USD Debit (Virtual only) AE",
+    sku: 18674,
+  },
+];
+
+export const visaInternationalSkus = [
+  {
+    country: "United States",
+    countryCode: "US",
+    name: "Visa International USD (Virtual) US",
+    sku: 18598,
+  },
+  {
+    country: "Algeria",
+    countryCode: "DZ",
+    name: "Virtual Promotional Prepaid Visa USD DZ",
+    sku: 18601,
+  },
+  {
+    country: "Australia",
+    countryCode: "AU",
+    name: "Virtual Promotional Prepaid Visa USD AU",
+    sku: 18602,
+  },
+  {
+    country: "Bangladesh",
+    countryCode: "BD",
+    name: "Virtual Promotional Prepaid Visa USD BD",
+    sku: 18603,
+  },
+  {
+    country: "Belgium",
+    countryCode: "BE",
+    name: "Virtual Promotional Prepaid Visa USD BE",
+    sku: 18604,
+  },
+  {
+    country: "Brazil",
+    countryCode: "BR",
+    name: "Virtual Promotional Prepaid Visa USD BR",
+    sku: 18605,
+  },
+  {
+    country: "Canada",
+    countryCode: "CA",
+    name: "Virtual Promotional Prepaid Visa USD CA",
+    sku: 18606,
+  },
+  {
+    country: "Dominica",
+    countryCode: "DM",
+    name: "Virtual Promotional Prepaid Visa USD DM",
+    sku: 18607,
+  },
+  {
+    country: "Egypt",
+    countryCode: "EG",
+    name: "Virtual Promotional Prepaid Visa USD EG",
+    sku: 18608,
+  },
+  {
+    country: "Finland",
+    countryCode: "FI",
+    name: "Virtual Promotional Prepaid Visa USD FI",
+    sku: 18609,
+  },
+  {
+    country: "France",
+    countryCode: "FR",
+    name: "Virtual Promotional Prepaid Visa USD FR",
+    sku: 18610,
+  },
+  {
+    country: "Georgia",
+    countryCode: "GE",
+    name: "Virtual Promotional Prepaid Visa USD GE",
+    sku: 18611,
+  },
+  {
+    country: "Greece",
+    countryCode: "GR",
+    name: "Virtual Promotional Prepaid Visa USD GR",
+    sku: 18612,
+  },
+  {
+    country: "India",
+    countryCode: "IN",
+    name: "Virtual Promotional Prepaid Visa USD IN",
+    sku: 18613,
+  },
+  {
+    country: "Indonesia",
+    countryCode: "ID",
+    name: "Virtual Promotional Prepaid Visa USD ID",
+    sku: 18614,
+  },
+  {
+    country: "Italy",
+    countryCode: "IT",
+    name: "Virtual Promotional Prepaid Visa USD IT",
+    sku: 18615,
+  },
+  {
+    country: "Jamaica",
+    countryCode: "JM",
+    name: "Virtual Promotional Prepaid Visa USD JM",
+    sku: 18616,
+  },
+  {
+    country: "Jordan",
+    countryCode: "JO",
+    name: "Virtual Promotional Prepaid Visa USD JO",
+    sku: 18617,
+  },
+  {
+    country: "Mexico",
+    countryCode: "MX",
+    name: "Virtual Promotional Prepaid Visa USD MX",
+    sku: 18618,
+  },
+  {
+    country: "Morocco",
+    countryCode: "MA",
+    name: "Virtual Promotional Prepaid Visa USD MA",
+    sku: 18619,
+  },
+  {
+    country: "Netherlands",
+    countryCode: "NL",
+    name: "Virtual Promotional Prepaid Visa USD NL",
+    sku: 18620,
+  },
+  {
+    country: "Philippines",
+    countryCode: "PH",
+    name: "Virtual Promotional Prepaid Visa USD PH",
+    sku: 18621,
+  },
+  {
+    country: "Poland",
+    countryCode: "PL",
+    name: "Virtual Promotional Prepaid Visa USD PL",
+    sku: 18622,
+  },
+  {
+    country: "Qatar",
+    countryCode: "QA",
+    name: "Virtual Promotional Prepaid Visa USD QA",
+    sku: 18623,
+  },
+  {
+    country: "Saudi Arabia",
+    countryCode: "SA",
+    name: "Virtual Promotional Prepaid Visa USD SA",
+    sku: 18624,
+  },
+  {
+    country: "Spain",
+    countryCode: "ES",
+    name: "Virtual Promotional Prepaid Visa USD ES",
+    sku: 18625,
+  },
+  {
+    country: "Sri Lanka",
+    countryCode: "LK",
+    name: "Virtual Promotional Prepaid Visa USD LK",
+    sku: 18626,
+  },
+  {
+    country: "Taiwan",
+    countryCode: "TW",
+    name: "Virtual Promotional Prepaid Visa USD TW",
+    sku: 18627,
+  },
+  {
+    country: "United Arab Emirates",
+    countryCode: "AE",
+    name: "Virtual Promotional Prepaid Visa USD AE",
+    sku: 18628,
+  },
+  {
+    country: "United Kingdom",
+    countryCode: "GB",
+    name: "Virtual Promotional Prepaid Visa USD GB",
+    sku: 18629,
+  },
+];
+
+/**
+ * There are separate allow list for visa & mastercard
+ * But right now, both lists contain same countries
+ */
+export const countryAllowList = [
+  {
+    code: "GP",
+    name: "Guadeloupe",
+  },
+  {
+    code: "AO",
+    name: "Angola",
+  },
+  {
+    code: "KI",
+    name: "Kiribati",
+  },
+  {
+    code: "CA",
+    name: "Canada",
+  },
+  {
+    code: "CM",
+    name: "Cameroon",
+  },
+  {
+    code: "QA",
+    name: "Qatar",
+  },
+  {
+    code: "VU",
+    name: "Vanuatu",
+  },
+  {
+    code: "EC",
+    name: "Ecuador",
+  },
+  {
+    code: "AE",
+    name: "United Arab Emirates",
+  },
+  {
+    code: "KZ",
+    name: "Kazakhstan",
+  },
+  {
+    code: "GI",
+    name: "Gibraltar",
+  },
+  {
+    code: "ID",
+    name: "Indonesia",
+  },
+  {
+    code: "BG",
+    name: "Bulgaria",
+  },
+  {
+    code: "BE",
+    name: "Belgium",
+  },
+  {
+    code: "CO",
+    name: "Colombia",
+  },
+  {
+    code: "BR",
+    name: "Brazil",
+  },
+  {
+    code: "IT",
+    name: "Italy",
+  },
+  {
+    code: "EG",
+    name: "Egypt",
+  },
+  {
+    code: "KR",
+    name: "South Korea",
+  },
+  {
+    code: "ZA",
+    name: "South Africa",
+  },
+  {
+    code: "OM",
+    name: "Oman",
+  },
+  {
+    code: "NF",
+    name: "Norfolk Island",
+  },
+  {
+    code: "SI",
+    name: "Slovak Republic",
+  },
+  {
+    code: "SB",
+    name: "Solomon Islands",
+  },
+  {
+    code: "LU",
+    name: "Luxembourg",
+  },
+  {
+    code: "BV",
+    name: "Bouvet Island",
+  },
+  {
+    code: "GG",
+    name: "Guernsey",
+  },
+  {
+    code: "GD",
+    name: "Grenada",
+  },
+  {
+    code: "AW",
+    name: "Aruba",
+  },
+  {
+    code: "JM",
+    name: "Jamaica",
+  },
+  {
+    code: "GF",
+    name: "French Guiana",
+  },
+  {
+    code: "NO",
+    name: "Norway",
+  },
+  {
+    code: "LI",
+    name: "Liechtenstein",
+  },
+  {
+    code: "HK",
+    name: "Hong Kong",
+  },
+  {
+    code: "HU",
+    name: "Hungary",
+  },
+  {
+    code: "PE",
+    name: "Peru",
+  },
+  {
+    code: "AM",
+    name: "Armenia",
+  },
+  {
+    code: "NL",
+    name: "Netherlands",
+  },
+  {
+    code: "BN",
+    name: "Brunei Darussalam",
+  },
+  {
+    code: "BM",
+    name: "Bermuda",
+  },
+  {
+    code: "VI",
+    name: "Virgin Islands (U.S.)",
+  },
+  {
+    code: "MS",
+    name: "Montserrat",
+  },
+  {
+    code: "GY",
+    name: "Guyana",
+  },
+  {
+    code: "DJ",
+    name: "Djibouti",
+  },
+  {
+    code: "PL",
+    name: "Poland",
+  },
+  {
+    code: "DK",
+    name: "Denmark",
+  },
+  {
+    code: "FM",
+    name: "Federated States of Micronesia",
+  },
+  {
+    code: "GH",
+    name: "Ghana",
+  },
+  {
+    code: "AD",
+    name: "Andorra",
+  },
+  {
+    code: "PF",
+    name: "French Polynesia",
+  },
+  {
+    code: "LV",
+    name: "Latvia",
+  },
+  {
+    code: "AZ",
+    name: "Azerbaijan",
+  },
+  {
+    code: "TO",
+    name: "Tonga",
+  },
+  {
+    code: "CC",
+    name: "Cocos (Keeling) Islands",
+  },
+  {
+    code: "MR",
+    name: "Mauritania",
+  },
+  {
+    code: "IL",
+    name: "Israel",
+  },
+  {
+    code: "GB",
+    name: "UK",
+  },
+  {
+    code: "LT",
+    name: "Lithuania",
+  },
+  {
+    code: "CL",
+    name: "Chile",
+  },
+  {
+    code: "DE",
+    name: "Germany",
+  },
+  {
+    code: "IN",
+    name: "India",
+  },
+  {
+    code: "BW",
+    name: "Botswana",
+  },
+  {
+    code: "IS",
+    name: "Iceland",
+  },
+  {
+    code: "FI",
+    name: "Finland",
+  },
+  {
+    code: "BH",
+    name: "Bahrain",
+  },
+  {
+    code: "US",
+    name: "USA",
+  },
+  {
+    code: "AI",
+    name: "Anguilla",
+  },
+  {
+    code: "CZ",
+    name: "Czech Republic",
+  },
+  {
+    code: "ES",
+    name: "Spain",
+  },
+  {
+    code: "MT",
+    name: "Malta",
+  },
+  {
+    code: "SG",
+    name: "Singapore",
+  },
+  {
+    code: "DZ",
+    name: "Algeria",
+  },
+  {
+    code: "CY",
+    name: "Cyprus",
+  },
+  {
+    code: "YT",
+    name: "Mayotte",
+  },
+  {
+    code: "MP",
+    name: "Northern Mariana Islands",
+  },
+  {
+    code: "HR",
+    name: "Croatia",
+  },
+  {
+    code: "CK",
+    name: "Cook Islands",
+  },
+  {
+    code: "GL",
+    name: "Greenland",
+  },
+  {
+    code: "SA",
+    name: "Saudi Arabia",
+  },
+  {
+    code: "GT",
+    name: "Guatemala",
+  },
+  {
+    code: "MY",
+    name: "Malaysia",
+  },
+  {
+    code: "JO",
+    name: "Jordan",
+  },
+  {
+    code: "IE",
+    name: "Ireland",
+  },
+  {
+    code: "CX",
+    name: "Christmas Island",
+  },
+  {
+    code: "RO",
+    name: "Romania",
+  },
+  {
+    code: "NZ",
+    name: "New Zealand",
+  },
+  {
+    code: "SV",
+    name: "El Salvador",
+  },
+  {
+    code: "FR",
+    name: "France",
+  },
+  {
+    code: "KE",
+    name: "Kenya",
+  },
+  {
+    code: "DM",
+    name: "Dominica",
+  },
+  {
+    code: "MA",
+    name: "Morocco",
+  },
+  {
+    code: "TW",
+    name: "Taiwan",
+  },
+  {
+    code: "NU",
+    name: "Niue",
+  },
+  {
+    code: "JP",
+    name: "Japan",
+  },
+  {
+    code: "PH",
+    name: "Philippines",
+  },
+  {
+    code: "GE",
+    name: "Georgia",
+  },
+  {
+    code: "GU",
+    name: "Guam",
+  },
+  {
+    code: "PW",
+    name: "Palau",
+  },
+  {
+    code: "AU",
+    name: "Australia",
+  },
+  {
+    code: "AS",
+    name: "American Samoa",
+  },
+  {
+    code: "CR",
+    name: "Costa Rica",
+  },
+  {
+    code: "GM",
+    name: "Gambia",
+  },
+  {
+    code: "TH",
+    name: "Thailand",
+  },
+  {
+    code: "CW",
+    name: "Cura√ßao",
+  },
+  {
+    code: "MQ",
+    name: "Martinique",
+  },
+  {
+    code: "PR",
+    name: "Puerto Rico",
+  },
+  {
+    code: "BQ",
+    name: "Caribbean Netherlands",
+  },
+  {
+    code: "BZ",
+    name: "Belize",
+  },
+  {
+    code: "MW",
+    name: "Malawi",
+  },
+  {
+    code: "NE",
+    name: "Niger",
+  },
+  {
+    code: "SI",
+    name: "Slovenia",
+  },
+  {
+    code: "AT",
+    name: "Austria",
+  },
+  {
+    code: "MX",
+    name: "Mexico",
+  },
+  {
+    code: "LK",
+    name: "Sri lanka",
+  },
+  {
+    code: "PT",
+    name: "Portugal",
+  },
+  {
+    code: "FK",
+    name: "Falkland Islands",
+  },
+  {
+    code: "UY",
+    name: "Uruguay",
+  },
+  {
+    code: "IO",
+    name: "British Indian Ocean Territory",
+  },
+  {
+    code: "GR",
+    name: "Greece",
+  },
+  {
+    code: "VA",
+    name: "Vatican City",
+  },
+  {
+    code: "AQ",
+    name: "Antarctica",
+  },
+  {
+    code: "FO",
+    name: "Faroe Islands",
+  },
+  {
+    code: "EE",
+    name: "Estonia",
+  },
+  {
+    code: "TV",
+    name: "Tuvalu",
+  },
+  {
+    code: "TF",
+    name: "French Southern Territories",
+  },
+  {
+    code: "SE",
+    name: "Sweden",
+  },
+  {
+    code: "AG",
+    name: "Antigua and Barbuda",
+  },
+  {
+    code: "BO",
+    name: "Bolivia",
+  },
+  {
+    code: "GA",
+    name: "Gabon",
+  },
+  {
+    code: "TM",
+    name: "Turkmenistan",
+  },
+  {
+    code: "AR",
+    name: "Argentina",
+  },
+  {
+    code: "JE",
+    name: "Jersey",
+  },
+  {
+    code: "CV",
+    name: "Cabo Verde",
+  },
+  {
+    code: "HM",
+    name: "Heard Island and McDonald Islands",
+  },
+  {
+    code: "BD",
+    name: "Bangladesh",
+  },
+  {
+    code: "BT",
+    name: "Bhutan",
+  },
+  {
+    code: "FJ",
+    name: "Fiji",
+  },
+  {
+    code: "CH",
+    name: "Switzerland",
+  },
+  {
+    code: "MC",
+    name: "Monaco",
+  },
+];
