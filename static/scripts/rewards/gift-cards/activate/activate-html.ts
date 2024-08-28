4e6929b489de4a63f7dc4df4d7f2835e4b485a0a
EresDev <eresdev@hotmail.com>
2024-08-28 15:34:21 -0400

    refactor: remove redundant html element

--- static/scripts/rewards/gift-cards/activate/activate-html.ts
+++ static/scripts/rewards/gift-cards/activate/activate-html.ts
@@ -4,16 +4,14 @@ const html = String.raw;
 
 export function getGiftCardActivateInfoHtml(giftCard: GiftCard) {
   return html`
-    <div class="redeem-info-wrapper">
-      <div class="redeem-info">
-        <fieldset>
-          <legend>How to use redeem code?</legend>
-          <div class="instructions">
-            <p>${giftCard.redeemInstruction.concise}</p>
-            ${giftCard.redeemInstruction.concise != giftCard.redeemInstruction.verbose ? `<p>${giftCard.redeemInstruction.verbose}</p>` : ``}
-          </div>
-        </fieldset>
-      </div>
+    <div class="redeem-info">
+      <fieldset>
+        <legend>How to use redeem code?</legend>
+        <div class="instructions">
+          <p>${giftCard.redeemInstruction.concise}</p>
+          ${giftCard.redeemInstruction.concise != giftCard.redeemInstruction.verbose ? `<p>${giftCard.redeemInstruction.verbose}</p>` : ``}
+        </div>
+      </fieldset>
     </div>
   `;
 }
