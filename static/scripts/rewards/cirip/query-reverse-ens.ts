a3635c1e0a8cc2415c093af6fcca6258e72146d2
hh <hhio618@gmail.com>
2024-09-10 15:37:09 +0330

    fix: ens lookup

--- static/scripts/rewards/cirip/query-reverse-ens.ts
+++ static/scripts/rewards/cirip/query-reverse-ens.ts
@@ -1,6 +1,6 @@
 import { app } from "../app-state";
 import { useRpcHandler } from "../web3/use-rpc-handler";
-import { reverseEnsInterface } from "./ens-lookup";
+import { ethers } from "ethers";
 
 export async function queryReverseEns(address: string, networkId: number) {
   // Try to get the ENS name from localStorage
@@ -17,27 +17,13 @@ export async function queryReverseEns(address: string, networkId: number) {
     return cachedEnsName;
   } else {
     // If the ENS name is not in localStorage, fetch it from the API
-    const data = reverseEnsInterface.encodeFunctionData("getNames", [[address.substring(2)]]);
+    const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+    const ensName = await web3Provider.lookupAddress(address);
 
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        id: "1",
-        method: "eth_call",
-        params: [{ to: "0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C", data: data }, "latest"],
-      }),
-    });
-
-    if (!response.ok) {
+    if (ensName === null) {
       console.error("ENS lookup failed: API request failed");
       return "";
     }
-
-    const ensName = await response.text();
 
     // Store the ENS name in localStorage for future use
     localStorage.setItem(address, ensName);
