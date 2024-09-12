4fca642498ee60ce5ccf24a5a7afe7c17f1204bc
アレクサンダー.eth <4975670+0x4007@users.noreply.github.com>
2024-09-12 12:51:21 +0900

    Revert "optimistic view claim rendering"

--- static/scripts/rewards/web3/erc20-permit.ts
+++ static/scripts/rewards/web3/erc20-permit.ts
@@ -100,9 +100,15 @@ async function transferFromPermit(permit2Contract: Contract, app: AppState) {
 async function waitForTransaction(tx: TransactionResponse) {
   try {
     const receipt = await tx.wait();
+    viewClaimButton.onclick = () => {
+      window.open(`https://blockscan.com/tx/${receipt.transactionHash}`, "_blank");
+    };
 
-    console.log(receipt.transactionHash);
     toaster.create("success", `Claim Complete.`);
+    buttonController.showViewClaim();
+    buttonController.hideLoader();
+    buttonController.hideMakeClaim();
+    console.log(receipt.transactionHash);
 
     return receipt;
   } catch (error: unknown) {
@@ -134,14 +140,8 @@ export function claimErc20PermitHandlerWrapper(app: AppState) {
     const permit2Contract = new ethers.Contract(permit2Address, permit2Abi, signer);
     if (!permit2Contract) return;
 
-    const tx: TransactionResponse = await transferFromPermit(permit2Contract, app);
+    const tx = await transferFromPermit(permit2Contract, app);
     if (!tx) return;
-
-    viewClaimButton.onclick = () => window.open(`https://blockscan.com/tx/${tx.hash}`, "_blank");
-
-    buttonController.showViewClaim();
-    buttonController.hideLoader();
-    buttonController.hideMakeClaim();
 
     const receipt = await waitForTransaction(tx);
     if (!receipt) return;
