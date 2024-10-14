68f2a442b4c914308a73cd83efead1d1f48eb357
rndquu <rndquu@gmail.com>
2024-10-14 12:53:30 +0300

    ci: fix knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,8 +1,8 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["build/esbuild-build.ts", "static/scripts/rewards/init.ts", "static/scripts/ubiquity-dollar/init.ts"],
-  project: ["src/**/*.ts", "static/scripts/**/*.ts"],
+  entry: ["build/esbuild-build.ts", "static/scripts/rewards/init.ts", "static/scripts/ubiquity-dollar/init.ts", "static/scripts/shared/api.ts"],
+  project: ["src/**/*.ts", "static/scripts/**/*.ts", "shared/**/api-types.ts"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "lib/**/*"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
