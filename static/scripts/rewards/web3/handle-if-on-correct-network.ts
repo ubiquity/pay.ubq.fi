db22f567d6ff0b1c483c54382532c384fca86ffe
zugdev <zug.dev@proton.me>
2024-10-15 01:13:02 -0300

    feat: centralize button visibility responsibility and delete a lot of redundant code

--- static/scripts/rewards/web3/handle-if-on-correct-network.ts
+++
@@ -1,9 +0,0 @@
-import { buttonController } from "../toaster";
-
-export function handleIfOnCorrectNetwork(currentNetworkId: number, desiredNetworkId: number) {
-  if (desiredNetworkId === currentNetworkId) {
-    buttonController.showMakeClaim();
-  } else {
-    buttonController.hideMakeClaim();
-  }
-}
