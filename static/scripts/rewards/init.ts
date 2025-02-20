c47c6c1ce83cea97b72481d1276f14a70790c75c
zugdev <zug.dev@proton.me>
2025-02-20 01:39:25 -0300

    feat: github login button

--- static/scripts/rewards/init.ts
+++ static/scripts/rewards/init.ts
@@ -1,9 +1,11 @@
 import { app } from "./app-state";
+import { initializeAuth } from "./auth";
 import { initClaimGiftCard } from "./gift-cards/index";
 import { displayCommitHash } from "./render-transaction/display-commit-hash";
 import { fetchPermits } from "./render-transaction/fetch-permits";
 import { grid } from "./the-grid";
 
+initializeAuth();
 displayCommitHash();
 grid(document.getElementById("grid") as HTMLElement, gridLoadedCallback); // @DEV: display grid background
 fetchPermits(app).catch(console.error); // @DEV: read claim data from URL
